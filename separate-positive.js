0ae7b042a6558abfe65b46d64dbbd68eafae9d0f
Tuan Anh Le <anhtuanle2101@gmail.com>
2021-11-04 15:10:59 -0400

    separatePositive

--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,18 @@
 // add whatever parameters you deem necessary
-function separatePositive() {
+function separatePositive(nums) {
+    if (nums.length <= 1) return nums;
+    let left = 0;
+    let right = nums.length-1;
+    while (left < right){
+        if (nums[left] > 0) left++;
+        if (nums[right] < 0) right --;
+        if (left < right){
+            const temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+        }
+    }
+    return nums;
 }
+
+module.exports = separatePositive;
