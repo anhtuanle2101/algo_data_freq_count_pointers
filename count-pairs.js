8330e453f8a0496ea469a774c680fc2f475ede5b
Tuan Anh Le <anhtuanle2101@gmail.com>
2021-11-04 15:23:54 -0400

    countPairs

--- count-pairs.js
+++ count-pairs.js
@@ -1,3 +1,13 @@
 // add whatever parameters you deem necessary
-function countPairs() {
+function countPairs(nums, target) {
+    const map = new Map();
+    let count = 0;
+    for (let num of nums){
+        if (map.get(target-num)) count++;
+        const isPresent = map.get(num) || false;
+        map.set(num, true);
+    }
+    return count;
 }
+
+module.exports = countPairs;
