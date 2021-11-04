8c16663bc76d436f519e8522247d30e413340bc8
Tuan Anh Le <anhtuanle2101@gmail.com>
2021-11-04 14:20:35 -0400

    sameFrequency

--- same-frequency.js
+++ same-frequency.js
@@ -1,3 +1,23 @@
 // add whatever parameters you deem necessary
-function sameFrequency() {
+function sameFrequency(num1, num2) {
+    if ((num1/num2)>10 || (num2/num1)>10) return false;
+    const freqNum1 = freqCount2(num1);
+    const freqNum2 = freqCount2(num2);
+    for (let num of freqNum1.keys()){
+        if (freqNum1.get(num) != freqNum2.get(num)) return false;
+    }
+    return true;
 }
+
+function freqCount2(num){
+    const map = new Map();
+    while (num > 0){
+        const cur = num % 10;
+        num = Math.floor(num / 10);
+        const count = map.get(cur) || 0;
+        map.set(cur, count + 1);
+    }
+    return map;
+}
+
+module.exports = sameFrequency;
--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,11 @@
 // add whatever parameters you deem necessary
-function twoArrayObject() {
+function twoArrayObject(keys, values) {
+    const obj ={}
+    for (let i=0;i<keys.length;i++){
+        if (values[i]) obj[keys[i]] = values[i]
+        else obj[keys[i]] = null;
+    }
+    return obj;
 }
+
+module.exports = twoArrayObject;
