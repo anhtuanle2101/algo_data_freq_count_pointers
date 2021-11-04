0a879f4cb8dc5b3e55e20b5b4f38688e5f9d6c55
Tuan Anh Le <anhtuanle2101@gmail.com>
2021-11-04 15:18:48 -0400

    isSubsequence

--- is-subsequence.js
+++ is-subsequence.js
@@ -1,3 +1,18 @@
 // add whatever parameters you deem necessary
-function isSubsequence() {
+function isSubsequence(str1, str2) {
+    if (str2.length < str1.length) return  false;
+    let pointer1 = 0;
+    let pointer2 = 0;
+    while (pointer2 < str2.length){
+        if (str1[pointer1] == str2[pointer2]){
+            pointer1++;
+            pointer2++;
+        }else{
+            pointer2++;
+        }
+    }
+    if (pointer1 == str1.length) return true;
+    return false;
 }
+
+module.exports = isSubsequence;
