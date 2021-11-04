51273604336868d55f1b4c236bda94a06ab4ec03
Tuan Anh Le <anhtuanle2101@gmail.com>
2021-11-04 13:48:31 -0400

    averagePair

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,14 @@
 // add whatever parameters you deem necessary
-function averagePair() {
+function averagePair(nums, target) {
+    if (nums.length <= 1) return false;
+    let left = 0;
+    let right = nums.length-1;
+    while (left < right){
+        if ((nums[left]+nums[right])/2 === target) return true;
+        if ((nums[left]+nums[right])/2 > target) right--;
+        if ((nums[left]+nums[right])/2 < target) left++;
+    }
+    return false;
 }
+
+module.exports = averagePair;
